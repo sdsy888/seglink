c069a55d6ef04cae3362de1d75ed519fea4d61e9
sdsy888 <sdsy888@users.noreply.github.com>
2017-09-06 17:02:55 +0800

    modify the dataset generation code

--- datasets/icdar2015_to_tfrecords.py
+++ datasets/icdar2015_to_tfrecords.py
@@ -3,7 +3,7 @@ import numpy as np;
 import tensorflow as tf
 import util
 from dataset_utils import int64_feature, float_feature, bytes_feature, convert_to_example
-        
+
 
 def cvt_to_tfrecords(output_path , data_path, gt_path):
     image_names = util.io.ls(data_path, '.jpg')#[0:10];
@@ -19,7 +19,7 @@ def cvt_to_tfrecords(output_path , data_path, gt_path):
             path = util.io.join_path(data_path, image_name);
             print "\tconverting image: %d/%d %s"%(idx, len(image_names), image_name);
             image_data = tf.gfile.FastGFile(path, 'r').read()
-            
+
             image = util.img.imread(path, rgb = True);
             shape = image.shape
             h, w = shape[0:2];
@@ -29,15 +29,15 @@ def cvt_to_tfrecords(output_path , data_path, gt_path):
             gt_name = 'gt_' + image_name + '.txt';
             gt_filepath = util.io.join_path(gt_path, gt_name);
             lines = util.io.read_lines(gt_filepath);
-                
+
             for line in lines:
                 line = util.str.remove_all(line, '\xef\xbb\xbf')
                 gt = util.str.split(line, ',');
                 oriented_box = [int(gt[i]) for i in range(8)];
                 oriented_box = np.asarray(oriented_box) / ([w, h] * 4);
                 oriented_bboxes.append(oriented_box);
-                
-                xs = oriented_box.reshape(4, 2)[:, 0]                
+
+                xs = oriented_box.reshape(4, 2)[:, 0]
                 ys = oriented_box.reshape(4, 2)[:, 1]
                 xmin = xs.min()
                 xmax = xs.max()
@@ -47,20 +47,22 @@ def cvt_to_tfrecords(output_path , data_path, gt_path):
                 ignored.append(util.str.contains(gt[-1], '###'));
 
                 # might be wrong here, but it doesn't matter because the label is not going to be used in detection
-                labels_text.append(gt[-1]); 
+                labels_text.append(gt[-1]);
                 labels.append(1);
             example = convert_to_example(image_data, image_name, labels, ignored, labels_text, bboxes, oriented_bboxes, shape)
             tfrecord_writer.write(example.SerializeToString())
-        
+
 if __name__ == "__main__":
-    root_dir = util.io.get_absolute_path('~/dataset/ICDAR2015/Challenge4/')
-    output_dir = util.io.get_absolute_path('~/dataset/SSD-tf/ICDAR/')
+    root_dir = util.io.get_absolute_path('~/Dataset/ICDAR2015/TextLocalization/')
+    output_dir = util.io.get_absolute_path('~/Dataset/ICDAR2015/TextLocalization/')
     util.io.mkdir(output_dir);
 
-#     training_data_dir = util.io.join_path(root_dir, 'ch4_training_images')
-#     training_gt_dir = util.io.join_path(root_dir,'ch4_training_localization_transcription_gt')
-#     cvt_to_tfrecords(output_path = util.io.join_path(output_dir, 'icdar2015_train.tfrecord'), data_path = training_data_dir, gt_path = training_gt_dir)
+    # for train data
+    # training_data_dir = util.io.join_path(root_dir, 'train_image')
+    # training_gt_dir = util.io.join_path(root_dir,'train_gt')
+    # cvt_to_tfrecords(output_path = util.io.join_path(output_dir, 'icdar2015_train_tfrecord'), data_path = training_data_dir, gt_path = training_gt_dir)
 
-    test_data_dir = util.io.join_path(root_dir, 'ch4_test_images')
-    test_gt_dir = util.io.join_path(root_dir,'ch4_test_localization_transcription_gt')
-    cvt_to_tfrecords(output_path = util.io.join_path(output_dir, 'icdar2015_test.tfrecord'), data_path = test_data_dir, gt_path = test_gt_dir)
+    # for test data
+    test_data_dir = util.io.join_path(root_dir, 'test_image')
+    test_gt_dir = util.io.join_path(root_dir,'test_gt')
+    cvt_to_tfrecords(output_path = util.io.join_path(output_dir, 'icdar2015_test_tfrecord'), data_path = test_data_dir, gt_path = test_gt_dir)
